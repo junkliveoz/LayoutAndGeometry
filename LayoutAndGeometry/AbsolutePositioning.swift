4c2a0686fb48219c55304d65d51e35a078b354f1
Adam Sayer <asayer82@icloud.com>
2024-09-03 14:36:25 +1000

    Day 93 complete

---
+++ LayoutAndGeometry/AbsolutePositioning.swift
@@ -0,0 +1,18 @@
+//
+//  AbsolutePositioning.swift
+//  LayoutAndGeometry
+//
+//  Created by Adam on 3/9/2024.
+//
+
+import SwiftUI
+
+struct AbsolutePositioning: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AbsolutePositioning()
+}
---
+++ LayoutAndGeometry/CustomAlignmentGuide.swift
@@ -0,0 +1,18 @@
+//
+//  CustomAlignmentGuide.swift
+//  LayoutAndGeometry
+//
+//  Created by Adam on 3/9/2024.
+//
+
+import SwiftUI
+
+struct CustomAlignmentGuide: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomAlignmentGuide()
+}
